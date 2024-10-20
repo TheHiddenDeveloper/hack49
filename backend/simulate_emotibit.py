b6f29f288d0a3f9ea280d37c2240397e52871964
HweyTH <thhuy1101@gmail.com>
2024-10-20 18:21:59 -0400

    Update scripts to simulate data streaming from Emotibits and OpenBCI Headband.

---
+++ backend/simulate_emotibit.py
@@ -0,0 +1,38 @@
+import random 
+import time
+import math
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+"""
+Description: Simulating EDA, BPM, Temperature, Accelerometer, and Sp02 data from an imaginary EmotiBit.
+
+"""
+def simulate_emotibit_data():
+    while True:
+        # Simulate data for Emotobit:
+        eda = round(random.uniform(0.5, 5.0), 2)
+        heart_rate = random.randint(60,100)
+        temperature = round(random.uniform(36.0, 37.5), 2)
+
+        # Package data into a dictionary
+        data = {
+            "eda": eda,
+            "heart_rate": heart_rate,
+            "temperature": temperature
+        }
+
+        # Stream data into WebSocket
+        socketio.emit('Emotibit', data)
+        time.sleep(1) # Rate of data upate (per second)
+
+# Route to trigger the simulation
+@app.route('/start-emotibit')
+def start_emotibit():
+    socketio.start_background_task(simulate_emotibit_data)
+    return jsonify({"message": "EmotiBit started!"})
+
+        
---
+++ backend/simulate_openbci.py
@@ -0,0 +1,28 @@
+import math
+import random
+import time
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, emit 
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+def simulate_open_bci():
+    while True:
+        # Generate EGG data for 4 channels
+        egg_data = {
+            # simulate data as 250 hz (same as OpenBCI headband)
+            "channel 1": [random.uniform(-100, 100) for _ in range(250)], 
+            "channel 2": [random.uniform(-100, 100) for _ in range(250)], 
+            "channel 3": [random.uniform(-100, 100) for _ in range(250)],
+            "channel 4": [random.uniform(-100, 100) for _ in range(250)]
+        }
+
+        # Stream EGG data into WebSocket
+        socketio.emit('OpenBCI Headband', egg_data)
+        time.sleep(1) # Data is updated per second
+
+@app.route('/start_openbci')
+def start_openbci():
+    socketio.start_background_task(simulate_open_bci)
+    return jsonify({"message": "OpenBCI Headband started!"})
